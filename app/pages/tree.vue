8530dc3ddcd6388c6a13280b9479fc9f41b58677
Ahmed Hosny <ahmedhosny0094.ah@gmail.com>
2025-11-09 23:00:14 +0200

    adding tree page

--- app/pages/tree.vue
+++ app/pages/tree.vue
@@ -3,7 +3,7 @@
     <div class="relative flex flex-col items-center justify-center w-full px-4 sm:px-6 lg:px-10 lg:-mt-16">
       <!-- Download Button - Above tree on mobile, fixed on desktop -->
       <button
-        class="self-center mb-6 mt-6 -mr-14 lg:fixed lg:top-34 lg:left-20 bg-[#4A2E1E] hover:scale-105 text-[#F1C687] px-6 py-3 rounded-full transition-all duration-300 lg:z-[130] flex items-center gap-2"
+        class="self-center mb-6 mt-6 lg:fixed lg:top-34 lg:left-20 bg-[#4A2E1E] hover:scale-105 text-[#F1C687] px-6 py-3 rounded-full transition-all duration-300 lg:z-[130] flex items-center gap-2"
       >
         <span>PDF تحميل الشجرة الحائطية</span>
       </button>
